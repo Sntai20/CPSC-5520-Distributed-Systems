ef994ec667d9d829062fab3ea0f7656cb7ab7b18
Antonio Santana <ansantan@outlook.com>
2022-10-09 16:55:54 -0700

    Add members and start an election.

--- Lab2-Bully/member2.py
+++ Lab2-Bully/member2.py
@@ -5,16 +5,21 @@ This is free and unencumbered software released into the public domain.
 :Version: 000-01
 
 Expected output:
+JOIN ('cs2.seattleu.edu', 23600)
+HELLO to {'host': 'cs1.seattleu.edu', 'port': 10000}
+('OK', "Happy to meet you, ('10.124.72.21', 38594)")
+HELLO to {'host': 'cs2.seattleu.edu', 'port': 33313}
+failed to connect: {} [Errno 111] Connection refused
+HELLO to {'host': 'localhost', 'port': 23015}
+('OK', "Happy to meet you, ('127.0.0.1', 44338)")
 """
 from datetime import datetime
 from enum import Enum
 import pickle
-import selectors
 import socket
 import sys
 
 BUFFER_SIZE = 1024  # tcp receive buffer size
-CHECK_INTERVAL = 1
 
 class MessageName(Enum):
     """Enum of all the message names."""
@@ -23,45 +28,24 @@ class MessageName(Enum):
     COORDINATOR = 3
     PROBE = 4
 
-class State(Enum):
-    """
-    Enumeration of states a peer can be in for the Lab2 class.
-    """
-    QUIESCENT = 'QUIESCENT'  # Erase any memory of this peer
 
-    # Outgoing message is pending
-    SEND_ELECTION = 'ELECTION'
-    SEND_VICTORY = 'COORDINATOR'
-    SEND_OK = 'OK'
-
-    # Incoming message is pending
-    WAITING_FOR_OK = 'WAIT_OK'  # When I've sent them an ELECTION message
-    WAITING_FOR_VICTOR = 'WHO IS THE WINNER?'  # This one only applies to myself
-    WAITING_FOR_ANY_MESSAGE = 'WAITING'  # When I've done an accept on their connect to my server
-
-    def is_incoming(self):
-        """Categorization helper."""
-        return self not in (State.SEND_ELECTION, State.SEND_VICTORY, State.SEND_OK)
-
-class Lab2():
+class Member():
     """
     Lab2 is used to join a group and meet members.
     """
-    def __init__(self, next_birthday, student_id):
+    def __init__(self, gcd_host, gcd_port, next_birthday, student_id):
         """
         param: gcd_host localhost or 127.0.0.1
         param: gcd_port 23633
         param: next_birthday YYYY-MM-DD
         param: student_id 123456
-        gcd_address consists of gcd_host and gcd_port
-
         """
+        self.gcd_address = (gcd_host, gcd_port)
         days_to_birthday = (datetime.fromisoformat(next_birthday) - datetime.now()).days
         self.process_id = (days_to_birthday, int(student_id))
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect(self.gcd_address)
         self.members_list = []
-        self.states = {}
-        self.bully = None
-        self.selector = selectors.DefaultSelector()
         self.listener, self.listener_address = self.start_a_server()
 
     @staticmethod
@@ -92,77 +76,20 @@ class Lab2():
         message_data = (self.process_id, self.listener_address)
         message = MessageName(1).name, message_data
         print(f"This is the message we are sending {message}")
-        # Setup the socket here.
-        mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        mysocket.connect(GCD_ADDRESS)
-        self.members_list = self.message(mysocket, message , BUFFER_SIZE)
-        mysocket.close()
+        self.members_list = self.message(self.socket, message , BUFFER_SIZE)
+        self.socket.close()
 
-    def run(self):
-        """
-        Runs event loop
-        """
-
-        # register MY listening socket
-        self.selector.register(self.listener, selectors.EVENT_READ)
-
-        # selector loop
-        while True:
-            events = self.selector.select(CHECK_INTERVAL)
-
-            print(events)
-
-            for key, mask in events:
-                if key.fileobj == self.listener:  # accept peer
-                    print(f"key, mask {key}, {mask}")
-                    self.accept_peer()
-            #     elif mask and selectors.EVENT_READ:  # recv msg
-            #         self.receive_message(key.fileobj)
-            #     else:  # mask and selectors.EVENT_WRITE
-            #         self.send_message(key.fileobj)  # send msg
-            # self.check_timeouts()
-
-    def accept_peer(self):
-        """
-        Accept new TCP/IP connections from a peer (TCP handshake)
-        """
-        print('in accept_peer')
-        try:
-            peer, address = self.listener.accept()
-            print(f"{peer}: accepted {address})")
-            # self.set_state(State.WAITING_FOR_ANY_MESSAGE, peer)
-        # except socket_error as serr:
-        #     print(f"Accept failed {serr}")
-        except ConnectionRefusedError as e_exception:
-            print(f"peer : {e_exception}")
-        except socket.gaierror as e_exception:
-            print(f"Address-related error connecting to server: {e_exception}")
-        except socket.error as e_exception:
-            print(f"failed to connect: {e_exception}")
-
-    def start_election(self):
+    def meet_members(self):
         """
          The message to send to the other group members is just
          the text HELLO (also pickled, of course).
         """
-        print("Start an election with the members in the member list.")
-        for member_process_id_dictionary in self.members_list:
-            # Am I in this list?
-            if member_process_id_dictionary == self.process_id:
-                print(f"My Process ID {self.process_id}")
-                continue
+        for member_dictionary_item in self.members_list:
+            # member_dictionary_item
+            #member_host = self.my_dictionary[0:1]
+            #member_port = self.my_dictionary[1]
+            print(member_dictionary_item[0:1])
 
-            if member_process_id_dictionary[0] > self.process_id[0]:
-                print(f"Member's Process ID {member_process_id_dictionary} " +
-                    f"is higher than mine {self.process_id}. Send them a message.")
-            else:
-                print(f"Member's Process ID {member_process_id_dictionary} " +
-                    f"is lower than mine {self.process_id}. No need to send them a message.")
-
-            # print(f"Member Process ID: {member_process_id_dictionary}")
-            # member_days_to_birthday, member_student_id = member_process_id_dictionary
-            # print(member_days_to_birthday)
-            # print(member_student_id)
             # #member_host, member_port = dictionary_item['host'], dictionary_item['port']
             # try:
             #     my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -191,7 +118,7 @@ class Lab2():
         sock.sendall(pickle.dumps(send_data))
         received_response_data = sock.recv(buffer_size)
         return_message = pickle.loads(received_response_data)
-        # print(f"This is the message we are receiving {return_message}")
+        print(f"This is the message we are receiving {return_message}")
         return return_message
 
 if __name__ == '__main__':
@@ -204,9 +131,8 @@ if __name__ == '__main__':
     GCD_PORT = sys.argv[2]
     NEXT_BIRTHDAY = sys.argv[3]
     STUDENT_ID = sys.argv[4]
-    GCD_ADDRESS = GCD_HOST, int(GCD_PORT)
     # print(f"Using the following address: {GCD_HOST} {GCD_PORT} {NEXT_BIRTHDAY} {STUDENT_ID}")
 
-    lab2 = Lab2(NEXT_BIRTHDAY, STUDENT_ID)
+    lab2 = Member(GCD_HOST, int(GCD_PORT), NEXT_BIRTHDAY, STUDENT_ID)
     lab2.join_group()
-    lab2.start_election()
+    #lab2.meet_members()
--- Lab2-Bully/run.py
+++ Lab2-Bully/run.py
@@ -2,13 +2,30 @@
 import os
 import time
 
-print("python3 gcd2.py 23633")
-os.system('python3 gcd2.py >> log-gcd2.txt &')
+# Clearing the Screen
+os.system('clear')
 
-print("Wait 0.5 seconds")
-time.sleep(1)
+print("\nStart of gcd2\n")
+print("python3 Lab2-Bully/gcd2.py 23633")
+os.system('python3 Lab2-Bully/gcd2.py >> log-gcd2.txt &')
 
-print("python3 lab2.py localhost 23633 2022-12-01 1234567")
-os.system('python3 lab2.py localhost 23633 2022-12-01 1234567')
+print("Wait 0.25 seconds for the server to start listening")
+time.sleep(0.25)
+print("\nEnd of gcd2")
 
-os.system("nohup python3 gcd2.py > log-gcd2.txt")
+print("\n\n\nStart of Members\n")
+print("python3 Lab2-Bully/member1.py localhost 23633 2022-11-01 1234561")
+os.system('python3 Lab2-Bully/member1.py localhost 23633 2022-11-01 1234561')
+print("\n\n")
+print("python3 Lab2-Bully/member2.py localhost 23633 2022-12-01 1234568")
+os.system('python3 Lab2-Bully/member2.py localhost 23633 2022-12-01 1234568')
+print("\n\nEnd of Members")
+
+print("\n\n\nStart of lab2\n")
+print("python3 Lab2-Bully/lab2.py localhost 23633 2022-11-01 1234567")
+os.system('python3 Lab2-Bully/lab2.py localhost 23633 2022-11-01 1234567')
+print("\n\n\nEnd of lab2\n\n\n")
+
+print("Shutting down the gcd2 server.")
+os.system("nohup python3 Lab2-Bully/gcd2.py > log-gcd2.txt")
+os.system("kill $(lsof -i:23633)")
