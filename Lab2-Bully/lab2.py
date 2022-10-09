9b3787ceca0d7ccfac61fe6a09b08260fb255647
Antonio Santana <ansantan@outlook.com>
2022-10-09 12:28:40 -0700

    Working Join Group Read in params, send join, setup listener, receive members.

--- Lab2-Bully/lab2.py
+++ Lab2-Bully/lab2.py
@@ -13,38 +13,72 @@ failed to connect: {} [Errno 111] Connection refused
 HELLO to {'host': 'localhost', 'port': 23015}
 ('OK', "Happy to meet you, ('127.0.0.1', 44338)")
 """
+from datetime import datetime
+from enum import Enum
 import pickle
 import socket
 import sys
 
 BUFFER_SIZE = 1024  # tcp receive buffer size
 
+class MessageName(Enum):
+    """Enum of all the message names."""
+    JOIN = 1
+    ELECTION = 2
+    COORDINATOR = 3
+    PROBE = 4
+
+
 class Lab2():
     """
     Lab2 is used to join a group and meet members.
     """
-    def __init__(self, gcd_host, gcd_port):
+    def __init__(self, gcd_host, gcd_port, next_birthday, student_id):
+        """
+        param: gcd_host localhost or 127.0.0.1
+        param: gcd_port 23633
+        param: next_birthday YYYY-MM-DD
+        param: student_id 123456
+        """
         self.gcd_host = gcd_host
         self.gcd_port = gcd_port
         self.gcd_address = (self.gcd_host, self.gcd_port)
+        days_to_birthday = (datetime.fromisoformat(next_birthday) - datetime.now()).days
+        self.process_id = (days_to_birthday, int(student_id))
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.gcd_host, self.gcd_port))
-        self.my_dictionary = {}
-        self.my_list = []
-        # self.listener, self.listener_address = self.start_a_server()
+        self.socket.connect(self.gcd_address)
+        self.members_list = []
+        self.listener, self.listener_address = self.start_a_server()
+
+    @staticmethod
+    def start_a_server():
+        """
+        Start up a listening server, use 0 to bind to any open port,
+        set the backlog to 100, and do not block the socket.
+
+        return: A listener.
+        """
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.bind(('localhost', 0))
+        listener.listen(100)
+        listener.setblocking(False)
+        return listener, listener.getsockname()
 
     def join_group(self):
         """
-        Joins the incoming messages - expects only 'JOIN' messages
         ('JOIN', ((days_to_bd, su_id), (host, port)))
+
+        All messages are pickled and are a pair (message_name, message_data),
+        where message_name is the text of the message name (that is, one of
+        'JOIN', 'ELECTION', 'COORDINATOR', or 'PROBE') and the message_data
+        is specified in the protocol below or, if there is no message data,
+        use None. Message responses, when they exist, can either be just
+        pickled text or data as specified in the protocol below.
         """
-        join_token = 'JOIN'
-        days_to_bd = '10'
-        su_id = '55555'
-        message = f"'{join_token}', '(({days_to_bd}, {su_id}), ({self.gcd_host}, 0))'"
+        message_data = (self.process_id, self.listener_address)
+        message = MessageName(1).name, message_data
         print(f"This is the message we are sending {message}")
-        self.my_dictionary = self.message(self.socket, message , BUFFER_SIZE)
-        #print(f"Response is {self.my_dictionary}")
+        self.members_list = self.message(self.socket, message , BUFFER_SIZE)
         self.socket.close()
 
     def meet_members(self):
@@ -52,11 +86,11 @@ class Lab2():
          The message to send to the other group members is just
          the text HELLO (also pickled, of course).
         """
-        for dictionary_item in self.my_list:
-            self.my_dictionary = dictionary_item
+        for member_dictionary_item in self.members_list:
+            # member_dictionary_item
             #member_host = self.my_dictionary[0:1]
             #member_port = self.my_dictionary[1]
-            print(self.my_dictionary[0:1])
+            print(member_dictionary_item[0:1])
 
             # #member_host, member_port = dictionary_item['host'], dictionary_item['port']
             # try:
@@ -83,40 +117,24 @@ class Lab2():
         """
         Serialize the message using pickle.
         """
-        response = pickle.dumps(send_data)
-        print(f"This is the pickled message we are sending {response}")
-        sock.sendall(response)
-        second_message = sock.recv(buffer_size)
-        return_message = pickle.loads(second_message)
+        sock.sendall(pickle.dumps(send_data))
+        received_response_data = sock.recv(buffer_size)
+        return_message = pickle.loads(received_response_data)
         print(f"This is the message we are receiving {return_message}")
         return return_message
 
-    # @staticmethod
-    # def start_a_server():
-    #     try:
-    #         process_id, listener = message_data
-    #         listen_host, listen_port = listener
-    #         days_to_birthday, student_id = process_id
-    #     except (ValueError, TypeError):
-    #         raise ValueError('Malformed message data, expected '
-    #             + '((days_to_bd, su_id), (host, port))') from TypeError
-    #     # make sure that listen_host is localhost or equivalent
-    #     try:
-    #         listen_ip = socket.gethostbyname(listen_host)
-    #     except Exception as err:
-    #         raise ValueError(str(err)) from err
-    #     if not (isinstance(listen_port) is int and 0 < listen_port < 65_536):
-    #         raise ValueError('Invalid port number')
-
-
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Usage: python lab2.py GCDHOST GCDPORT")
+    if len(sys.argv) != 5:
+        print("Usage: python lab2.py GCDHOST GCDPORT YYYY-MM-DD SUID")
+        print("Birthday format is YYYY-MM-DD")
         sys.exit(1)
+    
     GCD_HOST = sys.argv[1]
     GCD_PORT = sys.argv[2]
-    print(f"Using the following address: {GCD_HOST} {GCD_PORT}")
+    NEXT_BIRTHDAY = sys.argv[3]
+    STUDENT_ID = sys.argv[4]
+    print(f"Using the following address: {GCD_HOST} {GCD_PORT} {NEXT_BIRTHDAY} {STUDENT_ID}")
 
-    lab2 = Lab2(GCD_HOST, int(GCD_PORT))
+    lab2 = Lab2(GCD_HOST, int(GCD_PORT), NEXT_BIRTHDAY, STUDENT_ID)
     lab2.join_group()
     #lab2.meet_members()
