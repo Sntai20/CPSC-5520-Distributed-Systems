69dded59e7158ccfdfba7014f9e2f3a37c529461
Antonio Santana <ansantan@outlook.com>
2022-10-15 15:52:37 -0700

    Start lab3.

---
+++ Lab3-Pub-Sub/run.py
@@ -0,0 +1,25 @@
+"""
+CPSC 5520, Seattle University
+This is free and unencumbered software released into the public domain.
+:Authors: Antonio Santana
+:Version: 000-01
+
+Simple program to run this lab.
+"""
+
+import os
+import time
+
+def run():
+    """Simple program to run this lab."""
+
+    # Clearing the Screen
+    os.system('clear')
+
+    print("\n\n\nStart of lab3\n")
+    print("python3 Lab3-Pub-Sub/lab3.py")
+    os.system('python3 Lab3-Pub-Sub/lab3.py')
+    print("\n\n\nEnd of lab3\n\n\n")
+
+if __name__ == '__main__':
+    run()
